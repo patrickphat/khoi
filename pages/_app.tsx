4873efdc5946e9f07dd4d74eca127467e35e13d9
Patrick <me@patrickphat.com>
2024-11-14 20:01:55 +0700

    Update site config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import 'styles/global.css'
 // import 'prismjs/themes/prism-okaidia.css'
 // global style overrides for notion
 import 'styles/notion.css'
+// import 'styles/folio2.css'
 // global style overrides for prism theme (optional)
 import 'styles/prism-theme.css'
 
